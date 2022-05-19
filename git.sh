a4d74feb2dc27e23548e7de9cad20f8a44aae399
fadelmuharam <fdlmhrm@gmail.com>
2022-05-19 04:23:57 -0400

    ${commit}

--- git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git init
-git add -A
-read -p "Please input commit message : " commit
-git commit -m '${commit}'
-echo "token =\n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"
-git push -u -f origin master
-
